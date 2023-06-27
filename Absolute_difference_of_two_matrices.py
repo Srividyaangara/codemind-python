03d6e55420f5fd2a3c3602ac889a5ad13db36d3d
Srividyaangara <96339999+Srividyaangara@users.noreply.github.com>
2023-06-27 15:57:43 +0530

    n=int(input()) mat1,mat2,mattot=[],[],[] for i in range(n):     mat1.append(list(map(int,input().split()))) for i in range(n):     mat2.append(list(map(int,input().split()))) for i in range(n):     l=[]     for j in range(n):         l.append(abs(mat1[i][j]-mat2[i][j]))     mattot.append(l) for i in mattot:     print(*i)

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,14 @@
+n=int(input())
+mat1,mat2,mattot=[],[],[]
+for i in range(n):
+    mat1.append(list(map(int,input().split())))
+for i in range(n):
+    mat2.append(list(map(int,input().split())))
+for i in range(n):
+    l=[]
+    for j in range(n):
+        l.append(abs(mat1[i][j]-mat2[i][j]))
+    mattot.append(l)
+for i in mattot:
+    print(*i)
+    
